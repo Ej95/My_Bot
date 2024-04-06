cbb6407fbfd11ef1cb998ea866805b37e35c8fab
Chisato_so_good <ej38158136@gmail.com>
2024-04-06 16:32:34 +0800

    接上OpenAI API 實現聊天功能
    gpt-3.5-turbo-instruct

--- cmds/react.py
+++ cmds/react.py
@@ -2,18 +2,20 @@ import discord
 import json
 from discord.ext import commands
 from core.classes import Cog_Extension
-import random
+from openai import AsyncOpenAI
 
-with open("data.json","r",encoding="utf8") as file:
+with open(file=r"data.json",encoding="utf8") as file:
     jfile = json.load(file)
+
 
 class React(Cog_Extension): #繼承 Cog 類別  
 
-
+    #回傳 AI 的回覆
     @commands.command()
-    async def pic(self,ctx):
-        r_pic = random.choice(jfile["picture"])
-        await ctx.send(r_pic)
+    async def chat(self,ctx,arg):
+        client = AsyncOpenAI(api_key=jfile["api_key"])
+        res = await client.completions.create(model="gpt-3.5-turbo-instruct",prompt=arg,max_tokens=2048) #等待回覆生成 使用 await 異步
+        await ctx.send("{}".format(str(res.choices[0].text)))
 
 
 async def setup(bot):
